b7f36e919af49639614c11c98e69e15efa6e6614
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-09-09 22:47:26 +0000

    add Get Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,4 @@
 $(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   function createTweetElement(tweet) {
     const $tweet = $(`<article class="tweet">
     <header>
@@ -55,14 +29,21 @@ $(function () {
       $("#tweets-container").append($tweet);
     }
   };
-  renderTweets(data);
+  
+// Ajax call to read the tweets from db
+  function loadTweets(){
+    $.get('/tweets').then(data => {
+      renderTweets(data);
+    })
+  }
+  loadTweets()
 
+  //Ajax call to post the new tweet
   $("form#tweet-form").on("submit", function(event){
     event.preventDefault();
     const data = $( "form" ).serialize();
     $.post('/tweets/', data).then((response)=>{
       console.log(response);
     }) 
-  
   });
 });
